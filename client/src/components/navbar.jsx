3e93879c403e8d26a0cd69a7a33fd1eb0bc66693
Jesstrej <jessica16tor@yahoo.com>
2018-09-25 20:48:09 -0700

    update

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './navbar.style.css'
+
+
+
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( <div className = "text-center-navbar navbar-light bg-light customnavbar">
+            <h1> Plugged-Music </h1>
+        </div> );
+    }
+}
+ 
+export default Navbar;
--- server.js
+++ server.js
@@ -1,34 +1,48 @@
-require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const model = require('./models');
-
-const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+// Npm require ========================
+
+// EXPORT START ===========================
+const model = require('./models');
+// EXPORT END ===========================
 
 const port = process.env.PORT || 3001;
 
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to Mongodb");
-}).catch(err => {
-    console.log("Not connected");
-    console.error(err);
+// MONGO CONNECTION START ========================
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongo connected')
 });
+mongoose.connect('mongodb://localhost/test',{ useNewUrlParser: true });
+// heroku deployment
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.DATABASE, {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log("Successfully connected to Mongodb");
+// }).catch(err => {
+//     console.log("Not connected");
+//     console.error(err);
+// });
+// MONGO CONNECTION END ========================
 
+// MIDDLEAWRE =======================
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: true,
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+// MIDDLEAWRE =======================
 
-
-//user
+// API START =========================
 app.use("/api",require("./routes"))
+// API END =========================
 
 app.listen(port, () => {
     console.log(`server runing at ${port}`);
