ff7e5072a6589ed445e0150595929fb08dc20c82
ehab moustafa <ehabmo36@gmail.com>
2018-10-02 18:46:57 -0700

    images

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import './App.css';
+import './App.css';
 import './components/navbar.style.css';
 import Navbar from './components/navbar';
 import Searchbar from './components/searchbar';
