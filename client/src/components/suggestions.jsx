844b06d09b4655fd4a7c6a8eb45b23003967c7c4
ehab moustafa <ehabmo36@gmail.com>
2018-10-04 18:00:06 -0700

    view

--- client/src/components/suggestions.jsx
+++ client/src/components/suggestions.jsx
@@ -8,6 +8,7 @@ const Suggestions = (props) => {
   const options = props.results.map(r => (
     <div key={r.artistId + r.artistNames}>
       {r.artistName}
+      <a href={r.artistViewUrl}>view in store</a>
     </div>
   ))
   return <div>{options}</div>
