8c8737ff9a02f331d7d853cf5b88e3f707e47aad
ehab moustafa <ehabmo36@gmail.com>
2018-10-04 20:05:38 -0700

    heroku hosting setup

--- server.js
+++ server.js
@@ -44,6 +44,17 @@ app.use(cookieParser());
 app.use("/api",require("./routes"))
 // API END =========================
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  
+  // Send every request to the React app
+  // Define any API routes before this runs
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
 app.listen(port, () => {
     console.log(`server runing at ${port}`);
 });
